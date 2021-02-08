01263397983e4996e384de77111318d4a2c4e920
abd90 <53491426+abd90@users.noreply.github.com>
2021-02-09 02:49:50 +0300

    test7

--- app/Item.php
+++ app/Item.php
@@ -38,10 +38,11 @@ class Item extends Model
 
     public function getImageFromGoogleDrive()
     {
-        return 'https://drive.google.com/thumbnail?id=' . collect(\Storage::disk('google')->listContents('/', false))->where('type', '=', 'file')
+        $image = collect(\Storage::disk('google')->listContents('/', false))->where('type', '=', 'file')
         ->where('filename', '=', pathinfo($this->image, PATHINFO_FILENAME))
         ->where('extension', '=', pathinfo($this->image, PATHINFO_EXTENSION))
         ->first();
+        return 'https://drive.google.com/thumbnail?id=' . $image[0];
     }
 
 }
